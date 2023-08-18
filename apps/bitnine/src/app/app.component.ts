41a149db35e72f22bce57052e7d16199ac936a0f
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2023-08-18 16:56:22 +0200

    Initial commit

---
+++ apps/bitnine/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bitnine-recruitment-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'bitnine';
+}
