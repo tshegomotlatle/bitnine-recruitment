afcf023b073aaa657e9f939b6809a8f541c52325
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2023-08-21 07:12:25 +0200

    💄 Palindrone UI update

--- libs/ui/src/lib/lib.routes.ts
+++ libs/ui/src/lib/lib.routes.ts
@@ -3,6 +3,8 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { TestComponent } from './test/test.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { PalindroneComponent } from './palindrone/palindrone.component';
+import { MissingNumberComponent } from './missing-number/missing-number.component';
 
 export const uiRoutes: Route[] = [
   /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
@@ -10,5 +12,7 @@ export const uiRoutes: Route[] = [
   // {path: "**", pathMatch: "full", redirectTo: "/Product"},
   {path: "Home", pathMatch: "full", component: HomeComponent},
   {path: "Login", pathMatch: "full", component: LoginComponent},
+  {path: "Question-6-1", pathMatch: "full", component: PalindroneComponent},
+  {path: "Question-6-2", pathMatch: "full", component: MissingNumberComponent},
   {path: "test", pathMatch: "full", component: TestComponent }
 ];
