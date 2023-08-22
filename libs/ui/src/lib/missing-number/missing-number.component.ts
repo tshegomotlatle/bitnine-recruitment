9a522a96dad767688521fc1597b5fd16ae785a64
tshegomotlatle <tshegomotlatle87@gmail.com>
2023-08-22 09:25:16 +0200

    🐛 fix missing number bug

--- libs/ui/src/lib/missing-number/missing-number.component.ts
+++ libs/ui/src/lib/missing-number/missing-number.component.ts
@@ -8,20 +8,33 @@ import { Component } from '@angular/core';
 export class MissingNumberComponent {
   value = 50;
   missingArray : number [] = [];
+  normalArray : number [] = []
   missingList = "";
-  missingNumber = "";
+  missingNumber = -1;
 
   generate(){
+    this.missingArray = [];
+    this.normalArray = [];
     for (let k = 1; k <= this.value; k++) {
       this.missingArray.push(k)
+      this.normalArray.push(k)
     }
     this.missingArray.splice((Math.random() * this.value), 1)
     this.missingList = this.missingArray.join(", ")
   }
 
   findMissing(){
-    for (let k = 0; k < this.value; k++) {
-      
-    }
+    const sum = this.normalArray.reduce((a: number, b :number) =>{
+      return a + b
+    },0);
+    const sumValues = this.missingArray.reduce((a: number, b :number) =>{
+      return a + b
+    },0);
+    
+    this.missingNumber = sum - sumValues;
+    console.log("sum :" + sum);
+    console.log("sumValues :" + sumValues);
+    console.log("missingNumber :" + this.missingNumber);
+    
   }
 }
